8b258daae4f3b4025db1c6d461f279959e42a88d
Debidatta <debidattaray@gmail.com>
2021-05-07 13:22:08 +0530

    Favorite product

--- my_ecommerce/lib/main.dart
+++ my_ecommerce/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (ctx) => Products(),
-      //
       child: MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
--- my_ecommerce/lib/widgets/products_grid.dart
+++ my_ecommerce/lib/widgets/products_grid.dart
@@ -12,10 +12,15 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c) => products[i],
+        child: ProductItem(
+            // To add favorite status provider with single product
+            //(single products[i] instace created for every single product)
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
